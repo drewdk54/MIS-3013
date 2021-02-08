1380b961e016169748f3a8a14d985aa5eee75675
Drew Karnig <Andrew.L.Karnig-1@ou.edu>
2021-02-08 13:41:12 -0600

    started

--- In_Class_Example/Variables/Program.cs
+++ In_Class_Example/Variables/Program.cs
@@ -6,7 +6,23 @@ namespace Variables
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int heresMyInt;
+			double heresMyDouble;
+
+			Console.WriteLine($"Please eneter a double >>");
+			heresMyDouble = 5.5;
+			heresMyInt = 5;
+
+			Console.WriteLine($"Please enter an integer >>");
+			answer = Console.ReadLine();
+
+
+			double result = heresMyDouble + heresMyInt;
+
+			Console.WriteLine("The result of " + heresMyDouble + "+" + heresMyInt = "=" + result.ToString
+				("C5"));
+	
+
 		}
 	}
 }
